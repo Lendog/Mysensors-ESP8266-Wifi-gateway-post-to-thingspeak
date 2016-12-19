fc8315e17d4b5d00ec774b5f36e377f80dd351a4
Lendog <Lendog95@hotmail.com>
2016-12-19 21:34:16 +1100

    Update Esp8266GatewayTempAndWind.ino

--- Esp8266GatewayTempAndWind.ino
+++ Esp8266GatewayTempAndWind.ino
@@ -20,34 +20,34 @@
  *
  * REVISION HISTORY
  * Version 1.0 - Henrik EKblad
- * Contribution by a-lurker and Anticimex, 
+ * Contribution by a-lurker and Anticimex,
  * Contribution by Norbert Truchsess <norbert.truchsess@t-online.de>
  * Contribution by Ivo Pullens (ESP8266 support)
- * 
+ *
  * DESCRIPTION
- * The EthernetGateway sends data received from sensors to the WiFi link. 
+ * The EthernetGateway sends data received from sensors to the WiFi link.
  * The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
  *
  * VERA CONFIGURATION:
- * Enter "ip-number:port" in the ip-field of the Arduino GW device. This will temporarily override any serial configuration for the Vera plugin. 
+ * Enter "ip-number:port" in the ip-field of the Arduino GW device. This will temporarily override any serial configuration for the Vera plugin.
  * E.g. If you want to use the defualt values in this sketch enter: 192.168.178.66:5003
  *
  * LED purposes:
- * - To use the feature, uncomment WITH_LEDS_BLINKING in MyConfig.h
+ * - To use the feature, uncomment any of the MY_DEFAULT_xx_LED_PINs in your sketch, only the LEDs that is defined is used.
  * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
  * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
- * - ERR (red) - fast blink on error during transmission error or recieve crc error  
- * 
- * See http://www.mysensors.org/build/ethernet_gateway for wiring instructions.
- * The ESP8266 however requires different wiring:
+ * - ERR (red) - fast blink on error during transmission error or recieve crc error
+ *
+ * See http://www.mysensors.org/build/esp8266_gateway for wiring instructions.
  * nRF24L01+  ESP8266
  * VCC        VCC
- * CE         GPIO4          
+ * CE         GPIO4
  * CSN/CS     GPIO15
  * SCK        GPIO14
  * MISO       GPIO12
  * MOSI       GPIO13
- *            
+ * GND        GND
+ *
  * Not all ESP8266 modules have all pins available on their external interface.
  * This code has been tested on an ESP-12 module.
  * The ESP8266 requires a certain pin configuration to download code, and another one to run code:
@@ -56,221 +56,125 @@
  * - Connect CH_PD via 10K resistor to VCC
  * - Connect GPIO2 via 10K resistor to VCC
  * - Connect GPIO0 via 10K resistor to VCC, and via switch to GND ('bootload switch')
- * 
+ *
   * Inclusion mode button:
  * - Connect GPIO5 via switch to GND ('inclusion switch')
- * 
+ *
  * Hardware SHA204 signing is currently not supported!
  *
- * Make sure to fill in your ssid and WiFi password below for ssid & pass.  (also add thingspeak key further down)
- * 
- * Big thanks to Yveaux and nikil511 for there original code
+ * Make sure to fill in your ssid and WiFi password below for ssid & pass.
  */
-#define NO_PORTB_PINCHANGES 
 
-#include <SPI.h>  
-
-#include <MySigningNone.h> 
-#include <MySigningAtsha204Soft.h>
-#include <MyTransportNRF24.h>
-#include <MyTransportRFM69.h>
 #include <EEPROM.h>
-#include <MyHwESP8266.h>
-#include <ESP8266WiFi.h>
+#include <SPI.h>
 
-#include <MyParserSerial.h>  
-#include <MySensor.h>  
-#include <stdarg.h>
-#include "GatewayUtil.h"
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+// Use a bit lower baudrate for serial prints on ESP8266 than default in MyConfig.h
+#define MY_BAUD_RATE 9600
+
+// Enables and select radio type (if attached)
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+#define MY_GATEWAY_ESP8266
+
+#define MY_ESP8266_SSID "MySSID"
+#define MY_ESP8266_PASSWORD "MyVerySecretPassword"
+
+// Enable UDP communication
+//#define MY_USE_UDP
+
+// Set the hostname for the WiFi Client. This is the hostname
+// it will pass to the DHCP server if not static.
+// #define MY_ESP8266_HOSTNAME "sensor-gateway"
+
+// Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
+//#define MY_IP_ADDRESS 192,168,178,87
+
+// If using static ip you need to define Gateway and Subnet address as well
+#define MY_IP_GATEWAY_ADDRESS 192,168,178,1
+#define MY_IP_SUBNET_ADDRESS 255,255,255,0
+
+// The port to keep open on node server mode
+#define MY_PORT 5003
+
+// How many clients should be able to connect to this gateway (default 1)
+#define MY_GATEWAY_MAX_CLIENTS 4
+
+// Controller ip address. Enables client mode (default is "server" mode).
+// Also enable this if MY_USE_UDP is used and you want sensor data sent somewhere.
+//#define MY_CONTROLLER_IP_ADDRESS 192, 168, 178, 68
+
+// Enable inclusion mode
+#define MY_INCLUSION_MODE_FEATURE
+
+// Enable Inclusion mode button on gateway
+// #define MY_INCLUSION_BUTTON_FEATURE
+// Set inclusion mode duration (in seconds)
+#define MY_INCLUSION_MODE_DURATION 60
+// Digital pin used for inclusion mode button
+#define MY_INCLUSION_MODE_BUTTON_PIN  3
+
+
+// Set blinking period
+// #define MY_DEFAULT_LED_BLINK_PERIOD 300
+
+// Flash leds on rx/tx/err
+// Led pins used if blinking feature is enabled above
+#define MY_DEFAULT_ERR_LED_PIN 16  // Error led pin
+#define MY_DEFAULT_RX_LED_PIN  16  // Receive led pin
+#define MY_DEFAULT_TX_LED_PIN  16  // the PCB, on board LED
+
+#if defined(MY_USE_UDP)
+  #include <WiFiUdp.h>
+#endif
+
+#include <ESP8266WiFi.h>
+#include <MySensors.h>
 
 // replace with your channel's thingspeak API key, 
 const char* host = "api.thingspeak.com";   // thingspeak host address
-const char *ssid =  "#####";    // cannot be longer than 32 characters!
-const char *pass =  "#####"; //
-
-#define INCLUSION_MODE_TIME 1 // Number of minutes inclusion mode is enabled
-#define INCLUSION_MODE_PIN  5 // Digital pin used for inclusion mode button
-
-#define RADIO_CE_PIN        4   // radio chip enable
-#define RADIO_SPI_SS_PIN    15  // radio SPI serial select
-
-#ifdef WITH_LEDS_BLINKING
-#define RADIO_ERROR_LED_PIN 7  // Error led pin
-#define RADIO_RX_LED_PIN    8  // Receive led pin
-#define RADIO_TX_LED_PIN    9  // the PCB, on board LED
-#endif
+//const char* thingspeak_key = "ThingspeakAPIWriteKey";   //    was used for just one single thingspeak channel.. but i now have multiple and handled below
+const char *ssid =  "GatewayName";    //  Wifi gateway name -  cannot be longer than 32 characters!  might not be needed as handled above
+const char *pass =  "GatewayPassword"; // Wifi gateway password     - might not be needed as handled above
 
 
-// NRFRF24L01 radio driver (set low transmit power by default) 
-MyTransportNRF24 transport(RADIO_CE_PIN, RADIO_SPI_SS_PIN, RF24_PA_LEVEL_GW);
-//MyTransportRFM69 transport;
-
-
-// Message signing driver (signer needed if MY_SIGNING_FEATURE is turned on in MyConfig.h)
-#ifdef MY_SIGNING_FEATURE
-MySigningNone signer;
-//MySigningAtsha204Soft signer;
-#endif
-
-// Hardware profile 
-MyHwESP8266 hw;
-
-// Construct MySensors library (signer needed if MY_SIGNING_FEATURE is turned on in MyConfig.h)
-// To use LEDs blinking, uncomment WITH_LEDS_BLINKING in MyConfig.h
-MySensor gw(transport, hw
-#ifdef MY_SIGNING_FEATURE
-    , signer
-#endif
-#ifdef WITH_LEDS_BLINKING
-  , RADIO_RX_LED_PIN, RADIO_TX_LED_PIN, RADIO_ERROR_LED_PIN
-#endif
-  );
-  
-
-#define IP_PORT 5003         // The port you want to open 
-#define MAX_SRV_CLIENTS 5    // how many clients should be able to telnet to this ESP8266
-
-// a R/W server on the port
-static WiFiServer server(IP_PORT);
-static WiFiClient clients[MAX_SRV_CLIENTS];
-static bool clientsConnected[MAX_SRV_CLIENTS];
-static inputBuffer inputString[MAX_SRV_CLIENTS];
-
-#define ARRAY_SIZE(x)  (sizeof(x)/sizeof(x[0]))
-
-
-void output(const char *fmt, ... )
-{
-  char serialBuffer[MAX_SEND_LENGTH];
-  va_list args;
-  va_start (args, fmt );
-  vsnprintf_P(serialBuffer, MAX_SEND_LENGTH, fmt, args);
-  va_end (args);
-  Serial.print(serialBuffer);
-  for (uint8_t i = 0; i < ARRAY_SIZE(clients); i++)
-  {
-    if (clients[i] && clients[i].connected())
-    {
-//       Serial.print("Client "); Serial.print(i); Serial.println(" write");
-       clients[i].write((uint8_t*)serialBuffer, strlen(serialBuffer));
-    }
-  }
-}
-
-void setup()  
-{ 
-  // Setup console
-  hw_init();
-
+void setup() {
   Serial.println(); Serial.println();
   Serial.println("ESP8266 MySensors Gateway");
   Serial.print("Connecting to "); Serial.println(ssid);
 
   checkwifi();
-  
-  setupGateway(INCLUSION_MODE_PIN, INCLUSION_MODE_TIME, output);
+}
 
-  // Initialize gateway at maximum PA level, channel 70 and callback for write operations
-  //Changed below line - added ESP so didn't have to moddifly gatewayutil.h
-  gw.begin(incomingMessageESP, 0, true, 0);
-  
-  // start listening for clients
-  server.begin();
-  server.setNoDelay(true);  
+void presentation() {
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("REST-api gateway to Thingspeak", "1.1");
+  // Present locally attached sensors here
 }
 
 
 void loop() {
-  gw.process();  
-  
-  checkButtonTriggeredInclusion();
-  checkInclusionFinished();
-
-  // Go over list of clients and stop any that are no longer connected.
-  // If the server has a new client connection it will be assigned to a free slot.
-  bool allSlotsOccupied = true;
-  for (uint8_t i = 0; i < ARRAY_SIZE(clients); i++)
-  {
-    if (!clients[i].connected())
-    {
-      if (clientsConnected[i])
-      {
-        Serial.print("Client "); Serial.print(i); Serial.println(" disconnected");
-        clients[i].stop();
-      }
-      //check if there are any new clients
-      if (server.hasClient())
-      {
-        clients[i] = server.available();
-        inputString[i].idx = 0;
-        Serial.print("Client "); Serial.print(i); Serial.println(" connected"); 
-        output(PSTR("0;0;%d;0;%d;Gateway startup complete.\n"),  C_INTERNAL, I_GATEWAY_READY);
-      }
-    }
-    bool connected = clients[i].connected();
-    clientsConnected[i] = connected;
-    allSlotsOccupied &= connected;
-  }
-  if (allSlotsOccupied && server.hasClient())
-  {
-    //no free/disconnected spot so reject
-    Serial.println("No free slot available");
-    WiFiClient c = server.available();
-    c.stop();
-  }
-  
-  // Loop over clients connect and read available data
-  for (uint8_t i = 0; i < ARRAY_SIZE(clients); i++)
-  {
-    while(clients[i].connected() && clients[i].available())
-    {
-      char inChar = clients[i].read();
-      if ( inputString[i].idx < MAX_RECEIVE_LENGTH - 1 )
-      { 
-        // if newline then command is complete
-        if (inChar == '\n')
-        {  
-          // a command was issued by the client
-          // we will now try to send it to the actuator
-          inputString[i].string[inputString[i].idx] = 0;
-    
-          // echo the string to the serial port
-          Serial.print("Client "); Serial.print(i); Serial.print(": "); Serial.println(inputString[i].string);
-          parseAndSend(gw, inputString[i].string);
-    
-          // clear the string:
-          inputString[i].idx = 0;
-          // Finished with this client's message. Next loop() we'll see if there's more to read.
-          break;
-        } else {  
-         // add it to the inputString:
-         inputString[i].string[inputString[i].idx++] = inChar;
-        }
-      } else {
-        // Incoming message too long. Throw away 
-        Serial.print("Client "); Serial.print(i); Serial.println(": Message too long");
-        inputString[i].idx = 0;
-        // Finished with this client's message. Next loop() we'll see if there's more to read.
-        break;
-      }
-    }
-  }
+  // Send locally attached sensors data here
 }
 
-void incomingMessageESP(const MyMessage &message) {
+void receive(const MyMessage &message) {
+ char convBuf[MAX_PAYLOAD*2+1];
 //  if (mGetCommand(message) == C_PRESENTATION && inclusionMode) {
 //  gw.rxBlink(3);
 //   } else {
 //  gw.rxBlink(1);
 //   }
    // Pass along the message from sensors to serial line
-   serial(PSTR("%d;%d;%d;%d;%d;%s\n"),message.sender, message.sensor, mGetCommand(message), mGetAck(message), message.type, message.getString(convBuf));
+ // removed for my sensor 2 test...  serial(PSTR("%d;%d;%d;%d;%d;%s\n"),message.sender, message.sensor, mGetCommand(message), mGetAck(message), message.type, message.getString(convBuf));
     
   if (message.type == V_TEMP) {   //V_TEMP, // S_TEMP. Temperature S_TEMP, S_HEATER, S_HVAC
-    thingspeak(message.sensor, message.getString(convBuf),"#####");  // add thingspeak key
+    thingspeak(message.sensor, message.getString(convBuf),"#######");    //thingspeak channel 1 API write key
   }
   if (message.type == V_WIND) {             //V_WIND, // S_WIND. Wind speed
-    thingspeak(message.sensor, message.getString(convBuf),"######");   // add thingspeak key
+    thingspeak(message.sensor, message.getString(convBuf),"#######");   //thingspeak channel 2 API write key
     //Serial.print("Vwind");Serial.print(message.sensor);Serial.println(message.getString(convBuf));
   }
 
@@ -335,3 +239,5 @@ void checkwifi(){
   Serial.println("Connected!");  Serial.print("IP: "); Serial.println(WiFi.localIP());  Serial.flush();
   }
 }
+
+
